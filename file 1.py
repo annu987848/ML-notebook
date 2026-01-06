1c5adb0f9e20350048f3ae92d24d64f15804a7b2
Annu Pandey <rajive0andey@gmail.com>
2026-01-06 23:08:22 +0530

    Add greeting and total price calculation
    python file 1

---
+++ file 1.py
@@ -0,0 +1,9 @@
+name="annu";
+country="India"
+print(f"Hello, {name}! Welcome to {country}.")
+print(f"Letter.format {name} from {country}.")
+print("Letter.format {} from {}.".format(name, country))
+price=49.99
+quantity=3
+total=price*quantity
+print(f"Total price for {quantity} items is ${total:.2f}.")
