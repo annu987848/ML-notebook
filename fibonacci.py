aa4740def1bcedc70961ea53c1cb63899a695e4c
Annu Pandey <rajive0andey@gmail.com>
2026-01-07 12:05:33 +0530

    Implement Fibonacci series and print values
    Add Fibonacci series generation and print values

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+# Fibonacci series:
+# the sum of two elements defines the next
+a, b = 0, 1
+while a < 10:
+    print(a)
+    a, b = b, a+b
+i = 256*256
+print('The value of i is', i)
+The value of i is 65536
+a, b = 0, 1
+while a < 1000:
+    print(a, end=',')
+    a, b = b, a+b
