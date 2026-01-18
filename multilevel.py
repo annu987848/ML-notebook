a81be7efdaf70fa991fa5b04a346d29704af6a18
Annu Pandey <rajive0andey@gmail.com>
2026-01-18 20:53:48 +0530

    Define multilevel inheritance with dancer and employee

---
+++ multilevel.py
@@ -0,0 +1,15 @@
+class dancer:
+    pass
+class employee:
+    pass
+class manager(dancer, employee):
+    pass
+def __init__(self):
+    self.name="annu"
+def FunctionName(args):
+    function_body=""
+    pass
+o.paint()
+def paint(self):
+    print("painting")
+    
