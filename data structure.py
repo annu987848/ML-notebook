5f7a3fe520036b08dd055e103ae5afe5df72d7f3
Annu Pandey <rajive0andey@gmail.com>
2026-02-04 08:58:19 +0530

    Add various list and stack operations in Python

---
+++ data structure.py
@@ -0,0 +1,110 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+fruits.count('apple')
+
+fruits.count('tangerine')
+
+fruits.index('banana')
+
+fruits.index('banana', 4)  # Find next banana starting at position 4
+
+fruits.reverse()
+fruits
+
+fruits.append('grape')
+fruits
+
+fruits.sort()
+fruits
+
+fruits.pop()
+stack = [3, 4, 5]
+stack.append(6)
+stack.append(7)
+stack
+
+stack.pop()
+
+stack
+
+stack.pop()
+
+stack.pop()
+
+from collections import deque
+queue = deque(["Eric", "John", "Michael"])
+queue.append("Terry")           # Terry arrives
+queue.append("Graham")          # Graham arrives
+queue.popleft()                 # The first to arrive now leaves
+
+queue.popleft()                 # The second to arrive now leaves
+
+queue                           # Remaining queue in order of arrival
+squares = []
+for x in range(10):
+    squares.append(x**2)
+
+squares
+squares = list(map(lambda x: x**2, range(10)))
+squares = [x**2 for x in range(10)]
+[(x, y) for x in [1,2,3] for y in [3,1,4] if x != y]
+combs = []
+for x in [1,2,3]:
+    for y in [3,1,4]:
+        if x != y:
+            combs.append((x, y))
+
+combs
+vec = [-4, -2, 0, 2, 4]
+# create a new list with the values doubled
+[x*2 for x in vec]
+
+# filter the list to exclude negative numbers
+[x for x in vec if x >= 0]
+
+# apply a function to all the elements
+[abs(x) for x in vec]
+
+# call a method on each element
+freshfruit = ['  banana', '  loganberry ', 'passion fruit  ']
+[weapon.strip() for weapon in freshfruit]
+
+# create a list of 2-tuples like (number, square)
+[(x, x**2) for x in range(6)]
+
+# the tuple must be parenthesized, otherwise an error is raised
+[x, x**2 for x in range(6)]
+  File "<stdin>", line 1
+    [x, x**2 for x in range(6)]
+     ^^^^^^^
+SyntaxError: did you forget parentheses around the comprehension target?
+# flatten a list using a listcomp with two 'for'
+vec = [[1,2,3], [4,5,6], [7,8,9]]
+[num for elem in vec for num in elem]
+vec = [-4, -2, 0, 2, 4]
+# create a new list with the values doubled
+[x*2 for x in vec]
+
+# filter the list to exclude negative numbers
+[x for x in vec if x >= 0]
+
+# apply a function to all the elements
+[abs(x) for x in vec]
+
+# call a method on each element
+freshfruit = ['  banana', '  loganberry ', 'passion fruit  ']
+[weapon.strip() for weapon in freshfruit]
+
+# create a list of 2-tuples like (number, square)
+[(x, x**2) for x in range(6)]
+
+# the tuple must be parenthesized, otherwise an error is raised
+[x, x**2 for x in range(6)]
+  File "<stdin>", line 1
+    [x, x**2 for x in range(6)]
+     ^^^^^^^
+SyntaxError: did you forget parentheses around the comprehension target?
+# flatten a list using a listcomp with two 'for'
+vec = [[1,2,3], [4,5,6], [7,8,9]]
+[num for elem in vec for num in elem]
+
+stack
